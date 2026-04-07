47bfd1beebc79fe514248e1ad736f0698c88458b
Vincent Billey <vincent.billey@gmail.com>
2026-04-07 11:26:33 +0200

    fix(nvim): improve markdown code block readability in tokyonight-day
    Override RenderMarkdownCode and @markup.raw.markdown_inline to use
    bg_highlight instead of bg_dark for better contrast on light theme.

--- config/nvim/lua/plugins/colortheme.lua
+++ config/nvim/lua/plugins/colortheme.lua
@@ -2,8 +2,15 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
-    config = function() 
-      vim.cmd[[colorscheme tokyonight-day]]
-    end
+    opts = {
+        on_highlights = function(hl, c)
+            hl.RenderMarkdownCode = { bg = c.bg_highlight }
+            hl["@markup.raw.markdown_inline"] = {
+                bg = c.bg_highlight, fg = c.blue,
+            }
+        end,
+    },
+    config = function()
+        vim.cmd[[colorscheme tokyonight-day]]
+    end,
 }
