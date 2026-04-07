73068354cb7dd76fce5684d48fbfc37909508340
Vincent Billey <vincent.billey@gmail.com>
2026-04-07 11:29:45 +0200

    fix(nvim): improve markdown code block readability in tokyonight-day
    Use terracotta foreground color for code blocks and inline code
    instead of the default bg_dark background. Call setup() properly
    so on_highlights callback fires.

--- config/nvim/lua/plugins/colortheme.lua
+++ config/nvim/lua/plugins/colortheme.lua
@@ -2,15 +2,13 @@ return {
     "folke/tokyonight.nvim",
     lazy = false,
     priority = 1000,
-    opts = {
-        on_highlights = function(hl, c)
-            hl.RenderMarkdownCode = { bg = c.bg_highlight }
-            hl["@markup.raw.markdown_inline"] = {
-                bg = c.bg_highlight, fg = c.blue,
-            }
-        end,
-    },
     config = function()
+        require("tokyonight").setup({
+            on_highlights = function(hl, c)
+                hl.RenderMarkdownCode = { fg = "#c47050" }
+                hl["@markup.raw.markdown_inline"] = { fg = "#c47050" }
+            end,
+        })
         vim.cmd[[colorscheme tokyonight-day]]
     end,
 }
